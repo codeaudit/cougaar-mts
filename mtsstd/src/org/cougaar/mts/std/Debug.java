f019dc0a44ddca10e4ff1fb3f365e66935f3319e
psharma <psharma>
2001-08-14 19:35:29 +0000

    fix flag property name

--- mtsstd/src/org/cougaar/mts/std/Debug.java
+++ mtsstd/src/org/cougaar/mts/std/Debug.java
@@ -5,5 +5,5 @@ public interface Debug
     public static final boolean DEBUG_TRANSPORT = 
 	Boolean.getBoolean("org.cougaar.message.transport.debug");
     public static final boolean DEBUG_FLUSH = 
-	Boolean.getBoolean("org.cougaar.message.transport.flush.debug");
+	Boolean.getBoolean("org.cougaar.message.transport.debug.flush");
 }
