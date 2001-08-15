0ecd0a69c9093aad6d9689d47cc8b3d65736c923
psharma <psharma>
2001-08-15 19:16:48 +0000

    correct obsolete comments

--- mtsstd/src/org/cougaar/mts/std/LoopbackMessageTransport.java
+++ mtsstd/src/org/cougaar/mts/std/LoopbackMessageTransport.java
@@ -15,7 +15,7 @@ import org.cougaar.core.society.MessageAddress;
 
 
 /**
- * This transpory handles all intra-node message traffic.  It can act
+ * This transport handles all intra-node message traffic.  It can act
  * as its own DestinationLink, since this transport only sees traffic
  * for one destination.  The cost function is minimal (0) for local
  * traffic, and maximal (Integer.MAX_VALUE) for any other traffic. */
@@ -30,31 +30,13 @@ class LoopbackMessageTransport
 	super(aspects);
     }
 
-    // NB:  No aspects here!  Is that the right thing to do?
     public DestinationLink getDestinationLink(MessageAddress address) {
 	if (link == null) {
-	    link = (DestinationLink) attachAspects(this, DestinationLink.class,
+	    link = (DestinationLink) attachAspects(this, 
+						   DestinationLink.class,
 						   this);
 	}
 	return link;
-    }
-
-    // DestinationLink interface
-    public int cost(Message msg) {
-	MessageAddress addr = msg.getTarget();
-	if (registry.isLocalClient(addr)) {
-	    return 0;
-	} else {
-	    return Integer.MAX_VALUE;
-	}
-    }
-	
-
-
-    public void forwardMessage(Message message) 
-	throws MisdeliveredMessageException
-    {
-	deliverer.deliverMessage(message);
     }
 
     public void registerClient(MessageTransportClient client) {
@@ -72,5 +54,28 @@ class LoopbackMessageTransport
 	return registry.isLocalClient(address);
     }
    
+
+
+
+    // DestinationLink interface
+
+    public int cost(Message msg) {
+	MessageAddress addr = msg.getTarget();
+	if (registry.isLocalClient(addr)) {
+	    return 0;
+	} else {
+	    return Integer.MAX_VALUE;
+	}
+    }
+	
+
+
+    public void forwardMessage(Message message) 
+	throws MisdeliveredMessageException
+    {
+	deliverer.deliverMessage(message);
+    }
+
+
 
 }
