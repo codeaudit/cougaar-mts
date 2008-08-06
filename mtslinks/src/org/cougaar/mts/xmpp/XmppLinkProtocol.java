accc0ae852aed9830233bdd91663a8809fc53aaf
jzinky <jzinky>
2008-08-06 21:01:28 +0000

    fix javadoc and copyright notice

--- mtslinks/src/org/cougaar/mts/xmpp/XmppLinkProtocol.java
+++ mtslinks/src/org/cougaar/mts/xmpp/XmppLinkProtocol.java
@@ -1,45 +1,9 @@
 /*
- * Copyright (c) 2007 BBN Technologies Corp.
- * 
- * Restricted Release - Code is proprietary to BBN Technologies Corp. and
- * is to be used only as part of the DARPA DTN Program. 
- * 
- * Permission to use, copy, modify, and distribute this software and its
- * documentation for any research purpose within the DARPA DTN Program
- * only is hereby granted, provided that  
- * (1) the above copyright notice and this permission appear in all
- * copies and in supporting documentation 
- * (2) the name of BBN Technologies Corp. not be used in advertising or
- * publicity pertaining to distribution of the software without specific,
- * prior written permission  
- * (3) all rights, title and interest in any modifications shall remain
- * with BBN technologies Corp. 
- * 
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted only within the DARPA DTN Program provided
- * that the following conditions are met: 
- * (1) Redistributions of source code must retain the above copyright
- * notice, this list of conditions, and the following disclaimer. 
- * (2) Redistribution in binary form must include this file containing
- * the above copyright notice, this list of conditions, and the following
- * disclaimer in the documentation and/or other materials provided with
- * the distribution. 
- * (3) The names of the authors may not be used to endorse or promote
- * products derived from this software without specific prior written
- * permission 
- * 
- * THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. 
- * 
- * This material is based upon work supported by the U. S. Army CECOM and
- * the Department of Defense under Contract No. W15P7T-06-C-P638.  Any
- * opinions, findings and conclusions or recommendations expressed in
- * this material are those of the author(s) and do not necessarily
- * reflect the views of U. S. Army CECOM and the Department of Defense. 
  *
- * $Id: XmppLinkProtocol.java,v 1.1 2008-08-06 20:56:28 jzinky Exp $
+ * Copyright 2008 by BBN Technologies Corporation
+ *
  */
+
 
 package org.cougaar.mts.xmpp;
 
@@ -69,11 +33,11 @@ import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 
 /**
  * Simplified preliminary implementation of a Cougaar link protocol
- * that uses DTN to send MTS messages.
+ * that uses jabber api (xmpp) to send MTS messages.
  */
 public class XmppLinkProtocol extends PollingStreamLinkProtocol<Chat> {
     /**
-     * Max time in seconds before a bundle will be dropped by the BPA
+     * Max time in seconds to wait for an ack
      */
     private static final int REPLY_EXPIRATION_SECS = 7;
 
@@ -102,8 +66,7 @@ public class XmppLinkProtocol extends PollingStreamLinkProtocol<Chat> {
     }
 
     /**
-     * We must have a connected, registered input connection, a connected
-     * output connection and a non-null servant
+     * We must have a connected, authenticated connection and a non-null servant
      */
     protected boolean isServantAlive() {
         return 
@@ -139,8 +102,8 @@ public class XmppLinkProtocol extends PollingStreamLinkProtocol<Chat> {
     }
     
     /**
-     * Send a base64'ized message to a buddy.  The payload can either be
-     * an MTS message or an ack.
+     * Send a base64'ized message to a buddy.  The message can either be
+     * an MTS AttributedMessage or an ack.
      */
     protected Chat processOutgoingMessage(URI destination, MessageAttributes message) 
             throws IOException {
